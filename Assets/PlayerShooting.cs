6a2afd15c52ecb8693771347602735f91933de3d
Garzhax <alandiazcabj12@gmail.com>
2024-11-25 14:49:41 -0300

    mecánica de disparo

--- Assets/PlayerShooting.cs
+++ Assets/PlayerShooting.cs
@@ -1,18 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerShooting : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject bulletPrefab; // Referencia al prefab del proyectil
+    public Transform firePoint; // Punto desde donde se disparará el proyectil
+    public float bulletSpeed = 10f; // Velocidad del proyectil
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1")) // Detecta clic izquierdo
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        // Instanciar el proyectil en el punto de disparo
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody rb = bullet.AddComponent<Rigidbody>(); // Agregar Rigidbody para movimiento
+        rb.useGravity = false; // Desactiva la gravedad
+        rb.velocity = firePoint.forward * bulletSpeed; // Mueve el proyectil hacia adelante
+
+        // Destruir el proyectil después de 3 segundos
+        Destroy(bullet, 3f);
     }
 }
